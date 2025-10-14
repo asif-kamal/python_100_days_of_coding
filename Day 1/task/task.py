57bc207d2d72b8d6c644ab1a08eea227c380f395
asif-kamal <asif.kamal1489@gmail.com>
2025-10-14 11:06:23 -0500

    initial commit

---
+++ Day 1/task/task.py
@@ -0,0 +1,4 @@
+print("Hello! We will create a band name by combining your town name with your pet name\n")
+town_name = input("What town do you live in?\n")
+pet_name = input("What is your pet name?\n")
+print("Your new band name is: " + town_name +" "+ pet_name)
