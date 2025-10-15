528280280a3192c51eec8448e2d9acc4cd62af66
asif-kamal <asif.kamal1489@gmail.com>
2025-10-15 13:17:44 -0500

    completed if else exercise

--- Day 3/If Else/task.py
+++ Day 3/If Else/task.py
@@ -1,2 +1,6 @@
 print("Welcome to the rollercoaster!")
 height = int(input("What is your height in cm? "))
+if height > 100:
+    print("You can ride the roller!")
+else:
+    print("Sorry, you can't ride the roller!")
