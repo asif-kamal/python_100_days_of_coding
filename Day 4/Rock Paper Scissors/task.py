23fd41dfbe15c0b364ab31b19511f85463fbf9a7
asif-kamal <asif.kamal1489@gmail.com>
2025-10-18 20:00:37 -0500

    completed rock-paper-scissors game

--- Day 4/Rock Paper Scissors/task.py
+++ Day 4/Rock Paper Scissors/task.py
@@ -1,3 +1,5 @@
+import random
+
 rock = '''
     _______
 ---'   ____)
@@ -24,3 +26,43 @@ scissors = '''
       (____)
 ---.__(___)
 '''
+def rock_paper_scissors():
+    choice = input("Choose among 'rock', 'paper', or 'scissors' to play: ").lower()
+    computer_choice = random.choice([rock, paper, scissors])
+
+    if choice == "rock":
+        if computer_choice == scissors:
+            print("Computer: \n" + computer_choice)
+            print("You: \n" + rock + "\nYou win!")
+        elif computer_choice == paper:
+            print("Computer: \n" + computer_choice)
+            print("You: \n" + rock + "\nYou lose!")
+        elif computer_choice == rock:
+            print("Computer: \n" + computer_choice)
+            print("You: \n" + rock + "\nDraw!")
+    elif choice == "paper":
+        if computer_choice == rock:
+            print("Computer: \n" + computer_choice)
+            print("You: \n" + paper + "\nYou win!")
+        elif computer_choice == paper:
+            print("Computer: \n" + computer_choice)
+            print("You: \n" + paper + "\nDraw!")
+        elif computer_choice == scissors:
+            print("Computer: \n" + computer_choice)
+            print("You: \n" + paper + "\nYou lose!")
+    elif choice == "scissors":
+        if computer_choice == paper:
+            print("Computer: \n" + computer_choice)
+            print("You: \n" + scissors + "\nYou win!")
+        elif computer_choice == rock:
+            print("Computer: \n" + computer_choice)
+            print("You: \n" + scissors + "\nYou lose!")
+        elif computer_choice == scissors:
+            print("Computer: \n" + computer_choice)
+            print("You: \n" + scissors + "\nDraw!")
+
+    play_again = input("Play again? Y/N: ").lower()
+    if play_again == "y":
+        rock_paper_scissors()
+
+rock_paper_scissors()
