f4d4acf81b72be54f058f682693c5c0889790f37
asif-kamal <asif.kamal1489@gmail.com>
2025-10-26 19:54:40 -0500

    completed step 3 of hangman

--- Day 7/task/main.py
+++ Day 7/task/main.py
@@ -1,29 +1,33 @@
 import random
-
 word_list = ["aardvark", "baboon", "camel"]
+
 chosen_word = random.choice(word_list)
-place_holder = len(chosen_word)
+print(chosen_word)
 
-display_at_the_start = ""
+placeholder = ""
+word_length = len(chosen_word)
+for position in range(word_length):
+    placeholder += "_"
+print(placeholder)
+
+display = ""
+all_guesses = []
+# TODO-1: - Use a while loop to let the user guess again.
+while display != chosen_word:
+    guess = input("Guess a letter: ").lower()
+
+    display = ""
+    for letter in chosen_word:
+        if letter == guess:
+            all_guesses.append(guess)
+# TODO-2: Change the for loop so that you keep the previous correct letters in display.
+
+    for letter in chosen_word:
+        if letter in all_guesses:
+            display += letter
+        else:
+            display += "_"
 
 
-for number in range(place_holder):
-    display_at_the_start += "_"
-
-#print(chosen_word)
-print(display_at_the_start)
-# TODO-1: Create a "placeholder" with the same number of blanks as the chosen_word
-
-guess = input("Guess a letter: \n").lower()
-
-# TODO-2: Create a "display" that puts the guess letter in the right positions and _ in the rest of the string.
-display_after_guess = ""
-
-for letter in chosen_word:
-    if letter == guess:
-        display_after_guess += letter
-    else:
-        display_after_guess += "_"
-
-print(display_after_guess)
+    print(display)
 
