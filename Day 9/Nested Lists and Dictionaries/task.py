3238e92e56fb718597d61aed321dc0c0bd9b7d09
asif-kamal <asif.kamal1489@gmail.com>
2025-10-29 21:03:08 -0500

    practice w/ nested lists and dictionaries

---
+++ Day 9/Nested Lists and Dictionaries/task.py
@@ -0,0 +1,10 @@
+travel_log = {
+    "France": ["Paris", "Lille", "Cannes"],
+    "Germany": ["Berlin", "Munchen"],
+}
+
+travel_log["France"][1] = "Charlemagne"
+print(travel_log)
+
+nested_list = ["A", "B", [1, 2, 3]]
+print(nested_list[2][1])
