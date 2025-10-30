85e8fc11aa89379c4a79169672e557ac521cfeba
asif-kamal <asif.kamal1489@gmail.com>
2025-10-29 19:45:43 -0500

    completed caaser cipher 3

--- Day 8/Caesar Cipher 3/main.py
+++ Day 8/Caesar Cipher 3/main.py
@@ -1,3 +1,4 @@
+from art import logo
 # TODO-1: Import and print the logo from art.py when the program starts.
 
 
@@ -5,28 +6,40 @@ alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n'
 
 # TODO-2: What happens if the user enters a number/symbol/space?
 
+def caesar():
+    print(logo)
 
-def caesar(original_text, shift_amount, encode_or_decode):
+    encode_or_decode = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
+    original_text = input("Type your message:\n").lower()
+    shift_amount = int(input("Type the shift number:\n"))
+
     output_text = ""
 
     for letter in original_text:
-        if encode_or_decode == "decode":
-            shift_amount *= -1
 
-        shifted_position = alphabet.index(letter) + shift_amount
-        shifted_position %= len(alphabet)
-        output_text += alphabet[shifted_position]
+        if letter in alphabet:
+            shifted_position = 0
+            if encode_or_decode == 'encode':
+                shifted_position = alphabet.index(letter) + shift_amount
+            if encode_or_decode == 'decode':
+                shifted_position = alphabet.index(letter) - shift_amount
+            shifted_position %= len(alphabet)
+            output_text += alphabet[shifted_position]
+        else:
+            output_text += letter
+
     print(f"Here is the {encode_or_decode}d result: {output_text}")
+    try_again = input("Want to try again? Type 'yes' or 'no'.\n").lower()
+    if try_again == "yes":
+        caesar()
 
 
 # TODO-3: Can you figure out a way to restart the cipher program?
 
 
-direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
-text = input("Type your message:\n").lower()
-shift = int(input("Type the shift number:\n"))
 
-caesar(original_text=text, shift_amount=shift, encode_or_decode=direction)
+
+caesar()
 
 
 
