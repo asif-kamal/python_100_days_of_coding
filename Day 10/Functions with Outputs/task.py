7725c2eba85adc7b8125deb7289bc7aa289ccc06
asif-kamal <asif.kamal1489@gmail.com>
2025-10-30 20:04:40 -0500

    completed titlecase function and added docstring

--- Day 10/Functions with Outputs/task.py
+++ Day 10/Functions with Outputs/task.py
@@ -1,4 +1,6 @@
 def format_name(f_name, l_name):
+    """function to titlecase first name and last name"""
+
     return f"{f_name} {l_name}".title()
 
 result = format_name(input("Enter first name: "), input("Enter last name: "))
