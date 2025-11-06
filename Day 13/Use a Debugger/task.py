fb685af25f98bb9be7ea5be5fb7dd4f76d0fe625
asif-kamal <asif.kamal1489@gmail.com>
2025-11-06 16:23:52 -0600

    completed debugger exercise

--- Day 13/Use a Debugger/task.py
+++ Day 13/Use a Debugger/task.py
@@ -3,13 +3,15 @@ import maths
 
 
 def mutate(a_list):
+    """The bug was that you were appending the new_item at the end of the for loop,
+    which resulted in one value being added to the b_list"""
     b_list = []
-    new_item = 0
+    # new_item = 0
     for item in a_list:
         new_item = item * 2
         new_item += random.randint(1, 3)
         new_item = maths.add(new_item, item)
-    b_list.append(new_item)
+        b_list.append(new_item)
     print(b_list)
 
 
